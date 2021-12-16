54d51a1840293cef3ba2578d324652b1664ba829
fishZH <jiangzhong91@gmail.com>
2021-12-16 14:54:26 +0800

    源码05

---
+++ src/main/java/cn/lessann/test/javaSE05/ArrayTest1.java
@@ -0,0 +1,10 @@
+package cn.lessann.test.javaSE05;
+
+public class ArrayTest1 {
+    public static void main(String[] args) {
+        // 声明了一个a数组
+        int[] a;
+        // 声明了一个x数组和一个y变量
+        int x[], y;
+    }
+}
---
+++ src/main/java/cn/lessann/test/javaSE05/ArrayTest2.java
@@ -0,0 +1,10 @@
+package cn.lessann.test.javaSE05;
+
+public class ArrayTest2 {
+    public static void main(String[] args) {
+        double[] x = {59.5, 60, 77, 145, 95};
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(x[i]);
+        }
+    }
+}
